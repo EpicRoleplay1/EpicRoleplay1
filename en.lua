db5bd1315bd7418982455f5922d26205da2a29f2
EpicRoleplay1 <100820636+EpicRoleplay1@users.noreply.github.com>
2022-03-09 23:44:16 +0100

    test
    test

---
+++ en.lua
@@ -0,0 +1,44 @@
+Locales['en'] = {
+    ['unlock_doors'] = "Press ~y~[E]~w~ to unlock the doors",
+    ['lock_doors'] = "Press ~y~[E]~w~ to lock the doors",
+    ['hack_systems'] = "Press ~y~[E]~w~ to hack the system",
+    ['hack_vault'] = "Press ~y~[E]~w~ to hack the nonsense",
+    ['setup_winch'] = "Press ~y~[E]~w~ to place the winch",
+    ['rapeal_window'] = "Press ~y~[E]~w~ to rapeal through the window",
+    ['cut_window'] = "Press ~y~[E]~w~ to cut the window",
+    ['cut_glass'] = "Press ~y~[E]~w~ to cut the glass",
+    ['fix_window'] = "Press ~y~[E]~w~ to fix the window",
+    ['attach_rope'] = "Press ~y~[E]~w~ tie the rope",
+    ['detach_rope'] = "Press ~y~[G]~w~ let go of the rope",
+    ['steal_painting'] = "Press ~y~[E]~w~ to steal the painting",
+    ['steal_egg'] = "Press ~y~[E]~w~ steal the egg",
+    ['contact_dealer'] = "Press ~y~[E]~w~ call the black market dealer",
+    ['sell_blackmarket'] = "Press ~y~[E]~w~ sell your valuables",
+    
+    ['robbery_cancelled'] = "The robbery has been cancelled",
+    ['not_enough'] = "There aren't enough police online right now",
+    ['time_bought'] = "You got some time. You have %s seconds until the alarm goes off",
+    ['already_robbed'] = "The Art Gallery has already been recently stolen",
+    ['being_robbed'] = "The art gallery is being stolen!",
+    ['dont_have'] = "You need a %s to do this",
+    ['blip_name'] = "Prado Museum",
+    ['currency'] = "$%s",
+    ['blackmarket'] = "Black Market in Art",
+        
+    ['time_left']    = "You have s and %s s seconds to turn off the alarm",
+    ['skill_failed']    = "You've failed the skill test",
+
+    ['prog_cut_hole'] = "Hole",
+    ['prog_set_winch'] = "Place the winch",
+    ['prog_contact_dealer'] = "Contacting the black market seller",
+    ['prog_steal_painting'] = "Stealing the painting",
+    ['prog_steal_egg'] = "Stealing the egg",
+    ['prog_hacking']    = "Hacking the system",
+
+    ['subject_heading']    = "Pick-up location",
+    ['dealer_loc']    = "Suspicious activity",
+    ['text_message']    = "I'll meet you at the delivery site, I can't stay long, you have 30 seconds",
+}
+
+-- E = ~INPUT_CONTEXT~
+-- G = ~INPUT_DETONATE~
